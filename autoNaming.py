7bd84c6d78e3eb7599814ef270cab6c0ee1aa445
Swapnilpatel01 <42020767+Swapnilpatel01@users.noreply.github.com>
2019-03-17 23:25:17 -0500

    Update autoNaming.py

--- autoNaming.py
+++ autoNaming.py
@@ -1,6 +1,6 @@
 import os
-os.chdir('enter your directory path here')
 path = 'enter the same directory path as above'
+os.chdir(path)
 dirs = os.listdir(path)
 i = 0
 for file in dirs:
@@ -9,43 +9,43 @@ for file in dirs:
     for file_name in range(1):
         if(file_ext == 'png'):
             i += 1
-            x = input('Name your .png file: ')
+            x = input('Name your ' + file_ext + ' file: ')
             new_list.append(x)
         elif(file_ext == 'py'):
             i += 1
-            x = input('Name your .py file: ')
+            x = input('Name your ' + file_ext + ' file: ')
             new_list.append(x)
         elif (file_ext == 'jpg'):
             i += 1
-            x = input('Name your .jpg file: ')
+            x = input('Name your ' + file_ext + ' file: ')
             new_list.append(x)
         elif (file_ext == 'xlsx'):
             i += 1
-            x = input('Name your .xlsx file: ')
+            x = input('Name your ' + file_ext + ' file: ')
             new_list.append(x)
         elif (file_ext == 'pdf'):
             i += 1
-            x = input('Name your .pdf file: ')
+            x = input('Name your ' + file_ext + ' file: ')
             new_list.append(x)
         elif (file_ext == 'docx'):
             i += 1
-            x = input('Name your .docx file: ')
+            x = input('Name your ' + file_ext + ' file: ')
             new_list.append(x)
         elif (file_ext == 'docx'):
             i += 1
-            x = input('Name your .docx file: ')
+             x = input('Name your ' + file_ext + ' file: ')
             new_list.append(x)
         elif (file_ext == 'zip'):
             i += 1
-            x = input('Name your .zip file: ')
+             x = input('Name your ' + file_ext + ' file: ')
             new_list.append(x)
         elif (file_ext == 'mov'):
             i += 1
-            x = input('Name your .mov file: ')
+            x = input('Name your ' + file_ext + ' file: ')
             new_list.append(x)
         elif (file_ext == 'csv'):
             i += 1
-            x = input('Name your .csv file: ')
+            x = input('Name your ' + file_ext + ' file: ')
             new_list.append(x)
         else:
             print('There are either no files in your directory or there no files with the implemented extensions')
